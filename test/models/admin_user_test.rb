0f42cefac772838a075773649bff03c175b33508
Andrew Pam <andrew@subscriptus.com.au>
2014-11-24 22:52:30 +1100

    Add AdminUser model tests

--- test/models/admin_user_test.rb
+++ test/models/admin_user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class AdminUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save an admin user without an email address" do
+    admin_user = AdminUser.new( :password => 'testpass' )
+    assert_not admin_user.save
+  end
+  test "should not save an admin user with an invalid email address" do
+    admin_user = AdminUser.new( :email => 'test@test', :password => 'testpass' )
+    assert_not admin_user.save
+  end
+  test "should not save an admin user without a password" do
+    admin_user = AdminUser.new( :email => 'test@example.com' )
+    assert_not admin_user.save
+  end
+  test "should not save an admin user with a short password" do
+    admin_user = AdminUser.new( :email => 'test@example.com', :password => 'test' )
+    assert_not admin_user.save
+  end
+  test "should save an admin user with a valid email address and password" do
+    admin_user = AdminUser.new( :email => 'test@example.com', :password => 'testpass' )
+    assert admin_user.save
+  end
 end
