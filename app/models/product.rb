68d32446fd62a472cb88d4a7df01d8ec8ee83ca1
Andrew Pam <andrew@subscriptus.com.au>
2015-04-15 23:14:20 +1000

    Fix thumbnail sizes and add alt tags

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ActiveRecord::Base
   has_and_belongs_to_many :offers
   accepts_nested_attributes_for :offers
   validates :name, presence: true, uniqueness: true
-  has_attached_file :image, styles: { thumb: '100x100>' }
+  has_attached_file :image, styles: { thumb: '100x100#' }, convert_options: { thumb: '-strip -quality 75' }
   validates_attachment_content_type :image, :content_type => /\Aimage\//
 end
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -4,6 +4,6 @@ class Publication < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates_presence_of :website
   validates_format_of :website, with: URI.regexp, :if => "website.present?"
-  has_attached_file :image, styles: { thumb: '100x100>' }
+  has_attached_file :image, styles: { thumb: '100x100#' }, convert_options: { thumb: '-strip -quality 75' }
   validates_attachment_content_type :image, :content_type => /\Aimage\//
 end
