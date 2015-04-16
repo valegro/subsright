510af4e42cf9f793bab5c35d8607034130afce53
Andrew Pam <andrew@subscriptus.com.au>
2015-04-17 03:23:15 +1000

    Validate quantity field values

--- app/models/offer_publication.rb
+++ app/models/offer_publication.rb
@@ -4,6 +4,7 @@ class OfferPublication < ActiveRecord::Base
   belongs_to :publication
   accepts_nested_attributes_for :publication
   validates_presence_of :publication, :quantity, :unit
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
   scope :by_publication_name, lambda { joins(:publication).order('publications.name') }
 
   UNITS = ['Week', ['Month', 'Month', {checked: true}], 'Year']
