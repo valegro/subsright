aa62fb0e52f1f3a18634bedf0d77ec23c28ad67d
Andrew Pam <andrew@subscriptus.com.au>
2015-06-01 17:13:52 +1000

    Only show useful fields in AdminUser view (exclude tokens)

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -20,6 +20,28 @@ ActiveAdmin.register AdminUser do
   filter :created_at
   filter :updated_at
 
+  show do
+    attributes_table do
+      row :name
+      row :email
+      row :unconfirmed_email
+      row :confirmation_sent_at
+      row :confirmed_at
+      row :reset_password_sent_at
+      row :remember_created_at
+      row :sign_in_count
+      row :current_sign_in_at
+      row :last_sign_in_at
+      row :current_sign_in_ip
+      row :last_sign_in_ip
+      row :failed_attempts
+      row :locked_at
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
   form do |f|
     f.inputs 'Admin Details' do
       f.input :name
