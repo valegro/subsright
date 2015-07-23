c38675da67cd27653e79f5f99289f34e592fb025
Andrew Pam <andrew@subscriptus.com.au>
2015-07-23 23:11:08 +1000

    Add "large" image style for products and publications

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ActiveRecord::Base
   accepts_nested_attributes_for :offers
   has_many :product_orders
   validates :name, presence: true, uniqueness: true
-  has_attached_file :image, styles: { thumb: '100x100#' }, convert_options: { thumb: '-strip -quality 75' }
+  has_attached_file :image, styles: { large: '1020x768>', thumb: '100x100#' },
+    convert_options: { thumb: '-strip -quality 75' }
   validates_attachment_content_type :image, content_type: %r{\Aimage/}
 end
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -6,6 +6,7 @@ class Publication < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :website, presence: true
   validates :website, format: { with: URI.regexp }, if: 'website.present?'
-  has_attached_file :image, styles: { thumb: '100x100#' }, convert_options: { thumb: '-strip -quality 75' }
+  has_attached_file :image, styles: { large: '1020x768>', thumb: '100x100#' },
+    convert_options: { thumb: '-strip -quality 75' }
   validates_attachment_content_type :image, content_type: %r{\Aimage/}
 end
